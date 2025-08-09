56089056c53c06412c64b635dc9255002f97a6e8
premraj Ramankutty <premrajcr@icloud.com>
2025-08-09 12:19:33 +0530

    Initial Commit

---
+++ OpenAPIPostApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OpenAPIPostApp
+//
+//  Created by Premraj C R on 09/08/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ OpenAPIPostAppTests/OpenAPIPostAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  OpenAPIPostAppTests.swift
+//  OpenAPIPostAppTests
+//
+//  Created by Premraj C R on 09/08/25.
+//
+
+import Testing
+@testable import OpenAPIPostApp
+
+struct OpenAPIPostAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
