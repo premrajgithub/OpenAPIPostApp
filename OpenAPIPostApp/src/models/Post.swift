5f2fc87e137fca7ba3efcce649228bfabc11c9f1
premraj Ramankutty <premrajcr@icloud.com>
2025-08-09 12:38:23 +0530

    model added

--- OpenAPIPostApp/src/models/Post.swift
+++ OpenAPIPostApp/src/models/Post.swift
@@ -1,6 +1,14 @@
+//
+//  Post.swift
+//  OpenAPIPostApp
+//
+//  Created by Premraj C R on 09/08/25.
+//
+
+
 import Foundation
 
-struct Post: Codable, Identifiable {
+struct Post: Codable {
     let userId: Int
     let id: Int
     let title: String
